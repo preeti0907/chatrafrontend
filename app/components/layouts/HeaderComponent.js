433a20948e5085ca0cc77edcbbda515e23a42aae
naveen-laptop <naveenkhatanagit@gmail.com>
2023-06-06 10:12:10 +0530

    ggg

--- app/components/layouts/HeaderComponent.js
+++ app/components/layouts/HeaderComponent.js
@@ -1,14 +1,11 @@
 "use client";
 import React from 'react'
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]';
 
 function HeaderComponent() {
-  const session = await getServerSession(authOptions)
 
 
   return (
-    <>{json.stringify(session)}</>
+    <></>
   )
 }
 
