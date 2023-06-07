33147620d0cab58738bfbab5d170d9d1203ffbd3
sapnanarang <49829110+sapnanarang@users.noreply.github.com>
2023-06-07 09:56:32 +0530

    hh

--- app/components/layouts/FooterComponent.js
+++ app/components/layouts/FooterComponent.js
@@ -2,7 +2,82 @@ import React from 'react'
 
 function FooterComponent() {
   return (
-    <></>
+    <>
+    <div class="footer">
+  <div class="container">
+    <div class="row footer-inner">
+      <div class="col-12 col-sm-12 col-md-4 col-lg-3 footer-logo">
+        <a href="#">  <img src="assets/images/footer-logo.png" alt="logo"/></a>
+      
+      </div>
+      <div class="col-12 col-sm-12 col-md-4 col-lg-3 footer1">
+
+         <h2>CHATRA HELP</h2>
+        <ul>
+        
+           <li> <a href="#">About Us</a> </li>
+            <li> <a href="#">Contact Us</a> </li>
+             <li> <a href="#">Join Us</a> </li>
+              <li> <a href="#">Study Groups</a> </li>
+               <li> <a href="#">Resource Center</a> </li>
+                <li> <a href="#">Events</a> </li>
+                 <li> <a href="#">Blog</a> </li>
+             
+        </ul>
+
+      </div>
+      <div class="col-12 col-sm-12 col-md-4 col-lg-3 footer2">
+
+         <h2>POLICIES</h2>
+         <ul>
+      
+           <li> <a href="#">Terms of Use</a> </li>
+            <li> <a href="#">Privacy Policy</a> </li>
+             <li> <a href="#">Join Us</a> </li>
+              <li> <a href="#">Study Groups</a> </li>
+               <li> <a href="#">Resource Center</a> </li>
+                <li> <a href="#">Events</a> </li>
+                 <li> <a href="#">Blog</a> </li>
+             
+        </ul>
+
+      </div>
+      <div class="col-12 col-sm-12 col-md-4 col-lg-3 footer3">  
+
+        <h2>CHANNELS</h2>
+
+       <ul>
+         
+           <li> <a href="#">Podcast</a> </li>
+            <li> <a href="#">Privacy Policy</a> </li>
+             <li> <a href="#">Join Us</a> </li>
+              <li> <a href="#">Study Groups</a> </li>
+               <li> <a href="#">Resource Center</a> </li>
+                <li> <a href="#">Events</a> </li>
+                 <li> <a href="#">Blog</a> </li>
+             
+            </ul>
+      </div>
+      <div class="col-12 col-sm-12 col-md-4 col-lg-3 footer4">
+
+        <h2>ADDRESS</h2>
+        <p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+
+        <ul>
+          <li>Email:<a href="mailto:example@gmail.com">Example@gmail.com</a></li>
+          <li>Phone:<a href="tel:9521306897">+91 9952130697</a></li>
+        </ul>
+
+        
+      </div>
+    </div>
+  </div>
+</div>
+ 
+    
+    
+    </>
   )
 }
 
--- app/components/layouts/HeaderComponent.js
+++ app/components/layouts/HeaderComponent.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]';
 
-import React from 'react'
+
 
 
 function HeaderComponent() {
@@ -20,7 +20,43 @@ function HeaderComponent() {
    }, []);
 
   return (
-    <></>
+    <>
+    
+    <header>
+      <div class="container">
+        <div class="row inner-header">
+          <div class="col-2 header-logo">
+            <img src="assets/images/header-logo.png" alt="logo-image"/>
+          </div>
+           <div class="col-6 search"> 
+            <form class="d-flex" role="search">
+               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                
+            </form>
+            </div>
+            <div class="col-4 profile">
+              <ul>
+                <li>
+                  <a href="#"><img src="assets/images/client1.png"/></a>
+                  <p>John</p>
+                </li>
+                <li>
+                  <a href="#"><img src="assets/images/header-icon.png"   /></a>
+                  <p>Notification</p>
+                </li>
+
+                <li>
+                  <a href="#">
+                  <img src="assets/images/header-icon2.png"   /></a>
+                     <p>Notification</p>
+                 
+                </li>
+              </ul>
+            </div>
+        </div>
+      </div>
+  </header>
+    </>
   )
 }
 
